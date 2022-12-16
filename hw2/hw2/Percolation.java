90ceed895f35e71b8c840ee4a806dfa4747d8b1e
gulugulubing <413153391@qq.com>
2022-12-16 17:57:27 +0800

    my sub

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,128 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
 
+public class Percolation {
+    private int scale;
+    private int numOfOpen;
+    private WeightedQuickUnionUF grid;
+    private boolean[] open;
+    private int[] min;
+    private boolean isPercolation = false;
+    public Percolation(int N) {
+        grid = new WeightedQuickUnionUF(N * N);
+        open = new boolean[N*N];
+        min = new int[N*N];
+        scale = N;
+        numOfOpen = 0;
+        for (int i = 0; i < N*N; i++) {
+            min[i] = i;
+        }
+    }
+
+    public void open(int row, int col) {
+        int index = row * scale + col;
+        open[index] = true;
+        numOfOpen = numOfOpen + 1;
+        unionOpen(row, col);
+    }
+
+    private void unionOpen(int row, int col) {
+        int index = row * scale + col;
+        int top;
+        int bottom;
+        int left;
+        int right;
+        int parent1 = grid.find(index);
+        int parent2;
+        if (row > 0) {
+            top = (row - 1) * scale + col;
+            parent1 = grid.find(index);
+            parent2 = grid.find(top);
+            if (open[top] == true) {
+                grid.union(index, top);
+                if (min[parent1]<min[parent2]) {
+                    min[grid.find(index)] = min[parent1];
+                } else {
+                    min[grid.find(index)] = min[parent2];
+                }
+            }
+        }
+        if (row < scale - 1) {
+            bottom = (row + 1) * scale + col;
+            parent1 = grid.find(index);
+            parent2 = grid.find(bottom);
+            if (open[bottom] == true) {
+                grid.union(index, bottom);
+                if (min[parent1]<min[parent2]) {
+                    min[grid.find(index)] = min[parent1];
+                } else {
+                    min[grid.find(index)] = min[parent2];
+                }
+            }
+        }
+        if (col > 0) {
+            left = row * scale + col - 1;
+            parent1 = grid.find(index);
+            parent2 = grid.find(left);
+            if (open[left] == true) {
+                grid.union(index, left);
+                if (min[parent1]<min[parent2]) {
+                    min[grid.find(index)] = min[parent1];
+                } else {
+                    min[grid.find(index)] = min[parent2];
+                }
+            }
+        }
+        if (col < scale - 1) {
+            right = row * scale + col + 1;
+            parent1 = grid.find(index);
+            parent2 = grid.find(right);
+            if (open[right] == true) {
+                grid.union(index, right);
+                if (min[parent1]<min[parent2]) {
+                    min[grid.find(index)] = min[parent1];
+                } else {
+                    min[grid.find(index)] = min[parent2];
+                }
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return open[row * scale + col];
+    }
+
+    /*
+    public boolean isFull(int row, int col) {
+        int index = row * scale + col;
+        for(int i = 0; i < scale; i++) {
+            if(grid.connected(index,i) && open[i] == true) {
+                if (row == scale - 1) {
+                    isPercolation = true;
+                }
+                return true;
+            }
+        }
+        return false;
+    }*/
+
+    public boolean isFull(int row, int col) {
+        int index = row * scale + col;
+        if (min[grid.find(index)] < scale && open[index]) {
+            if (row == scale - 1) {
+                isPercolation = true;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return numOfOpen;
+    }
+
+    public boolean percolates() {
+        return isPercolation;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,41 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private double[] result;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        result = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation per = pf.make(N);
+            while (!per.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                if (!per.isOpen(row, col)) {
+                    per.open(row, col);
+                }
+                per.isFull(row, col);
+            }
+            result[i] = ((double) per.numberOfOpenSites()) / ((double) (N * N));
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(result);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(result);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(result.length);
+    }
+
+    public double confidenceHigh() {
+        return  mean() + 1.96 * stddev() / Math.sqrt(result.length);
+    }
 
 }
