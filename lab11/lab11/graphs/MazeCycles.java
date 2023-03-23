c90e77286b47fe9423acbfe1bd316e8e48be9a34
gulugulubing <413153391@qq.com>
2023-03-23 22:10:02 +0800

    first

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,5 @@
 package lab11.graphs;
-
+import edu.princeton.cs.algs4.Stack;
 /**
  *  @author Josh Hug
  */
@@ -9,16 +9,60 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        int sourceX = 1;
+        int sourceY = 1;
+        int targetX = maze.N();
+        int targetY = maze.N();
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = -1;
+        edgeTo[s] = s;
+        dfs(s);
     }
 
-    // Helper methods go here
+    //use a stack not recursion to DEPTH FIRST PATH
+    private void dfs(int v) {
+        Stack<Integer> stack = new Stack();
+        int previous = 0;
+        int current;
+        stack.push(v);
+        while (!stack.isEmpty()) {
+            announce();
+            current = stack.pop();
+            if (!marked[current]) {
+                marked[current] = true;
+                distTo[current] = distTo[previous] + 1;
+            }
+            else {
+                //delete none-circle edge
+                for(int i = 0 ; i < maze.V() ; i++) {
+                    if (distTo[i] < distTo[current]) {
+                        edgeTo[i] = i;
+                    }
+                }
+                announce();
+                break;
+            }
+            for (int w : maze.adj(current)) {
+                if ( w != edgeTo[current] ) {
+                    edgeTo[w] = current;
+                    stack.push(w);
+                }
+            }
+            previous = current;
+        }
+    }
+
 }
 
